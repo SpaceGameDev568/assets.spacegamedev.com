2fd14603d39eee01f14d9b21ec43cf5f8e9720ff
SpaceGameDev568 <spacegamedev568@gmail.com>
2023-03-17 15:50:51 -0400

    Added SHA256 Checksum for Charles

--- projects/DoubleClickMe.bat
+++ projects/DoubleClickMe.bat
@@ -2,4 +2,6 @@ echo Copyright (c) 2023 SpaceGameDev568. All Rights Reserved.
 
 curl "https://assets.spacegamedev.com/projects/Charles.exe" --output "Charles.exe"
 
+curl "https://assets.spacegamedev.com/projects/SHA256CHECKSUM.txt" --output "SHA256CHECKSUM.txt"
+
 Charles sysinfo
