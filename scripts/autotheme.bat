5a376789e7a806836951a22ffd4211e3223da6b3
SpaceGameDev568 <spacegamedev568@gmail.com>
2023-04-15 11:27:03 -0400

    Update autotheme.bat

--- scripts/autotheme.bat
+++ scripts/autotheme.bat
@@ -2,6 +2,8 @@
 
 cmd /c powershell -Nop -NonI -Nologo -WindowStyle Hidden "Write-Host"
 
+:: Always revert theme
+
 powershell -Command "Set-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize -Name AppsUseLightTheme -Value 0"
 
 powershell -Command "Set-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize -Name SystemUsesLightTheme -Value 0"
