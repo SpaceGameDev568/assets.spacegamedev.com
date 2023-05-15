348ec777914d58e25566dc009d2b01847eae07dd
Jesse Hodgson <spacegamedev568@gmail.com>
2023-05-15 14:22:24 -0400

    Removed Unnecessary Files

--- scripts/autotheme.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-
-cmd /c powershell -Nop -NonI -Nologo -WindowStyle Hidden "Write-Host"
-
-:: Always revert theme
-
-powershell -Command "Set-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize -Name AppsUseLightTheme -Value 0"
-
-powershell -Command "Set-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize -Name SystemUsesLightTheme -Value 0"
