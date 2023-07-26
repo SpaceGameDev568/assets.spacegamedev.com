fd27674c267f7a61f1bbd1bc1494e6f70a898684
SpaceGameDev568 <space@spacegamedev.com>
2023-07-26 18:08:00 -0400

    Update batch files

---
+++ disable_telemetry.bat
@@ -0,0 +1,56 @@
+:: Copyright (©) 2023 Jesse Hodgson. All Rights Reserved.
+
+echo Hello
+
+:: BatchGotAdmin
+::-------------------------------------
+REM  --> Check for permissions
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+
+REM --> If error flag set, we do not have admin.
+if '%errorlevel%' NEQ '0' (
+    echo Requesting administrative privileges...
+    goto UACPrompt
+) else ( goto gotAdmin )
+
+:UACPrompt
+    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+    set params = %*:"="
+    echo UAC.ShellExecute "cmd.exe", "/c %~s0 %params%", "", "runas", 1 >> "%temp%\getadmin.vbs"
+
+    "%temp%\getadmin.vbs"
+    del "%temp%\getadmin.vbs"
+    exit /B
+
+:gotAdmin
+    pushd "%CD%"
+    CD /D "%~dp0"
+::--------------------------------------
+
+echo Hello there! >> "%USERPROFILE%\Desktop\ThisWasntHereBefore.txt"
+
+reg add "HKEY_CURRENT_USER\SOFTWARE\Policies\Microsoft\Windows\DataCollection" /v AllowTelemetry /t REG_SZ /d "0" /f
+sc config DiagTrack start= disabled
+
+reg add "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Search" /v BingSearchEnabled /t REG_SZ /d "0" /f
+
+reg add "HKEY_LOCAL_MACHINE\SYSTEM\Setup\LabConfig" /v BypassTPMCheck /t REG_SZ /d "1" /f
+reg add "HKEY_LOCAL_MACHINE\SYSTEM\Setup\LabConfig" /v BypassSecureBootCheck /t REG_SZ /d "1" /f
+reg add "HKEY_LOCAL_MACHINE\SYSTEM\Setup\LabConfig" /v BypassRAMCheck /t REG_SZ /d "1" /f
+
+powershell -Command "Set-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize -Name AppsUseLightTheme -Value 0"
+powershell -Command "Set-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize -Name SystemUsesLightTheme -Value 0"
+
+winget update --all
+
+cleanmgr.exe /sagerun:1&exit
+
+defrag.exe /C /D /G /H
+
+curl "https://github.com/SpaceGameDev568/Charles/releases/download/v0.1.1a/Charles.exe" --output "Charles.exe"
+
+Charles sysinfo
+
+echo Command completed! You may now close this window.
+
+pause
